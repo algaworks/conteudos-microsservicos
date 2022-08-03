58baa64a0bdf9f47b4312d3d9984b62b563b02b8
Alex Silva <alex.silva@algaworks.com>
2022-08-03 14:28:47 -0300

    Tipos de exchanges

---
+++ Mensageria/Spring AMQP - Tipos de Exchange/coupon-service/src/main/java/com/algaworks/example/coupon/model/OrderModel.java
@@ -0,0 +1,46 @@
+package com.algaworks.example.coupon.model;
+
+import java.math.BigDecimal;
+
+public class OrderModel {
+	private Long id;
+	private BigDecimal value = BigDecimal.ZERO;
+	private Boolean paid = Boolean.FALSE;
+
+	public OrderModel() {
+	}
+
+	public OrderModel(Long id, BigDecimal value, Boolean paid) {
+		this.id = id;
+		this.value = value;
+		this.paid = paid;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public boolean isPaid() {
+		return paid;
+	}
+
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
+
+	public void markAsPaid() {
+		this.paid = true;
+	}
+}
---
+++ Mensageria/Spring AMQP - Tipos de Exchange/order-service/src/main/java/com/algaworks/example/order/config/RabbitMQConfig.java
@@ -0,0 +1,41 @@
+package com.algaworks.example.order.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(
+            RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(ObjectMapper objectMapper) {
+        objectMapper.findAndRegisterModules();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+}
