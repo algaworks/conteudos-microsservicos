e60b79fe586afe38072900afa451c2fe3f87206f
Alex Silva <alexaugustoti@gmail.com>
2023-07-13 14:00:57 -0300

    Testes com spring security

--- Segurança/Spring Security - Como testar usando mocks - Final/src/test/java/com/algaworks/example/secure/ProductRestControllerTest.java
+++ Segurança/Spring Security - Como testar usando mocks - Final/src/test/java/com/algaworks/example/secure/ProductRestControllerTest.java
@@ -5,40 +5,42 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class ProductRestControllerTest {
+public class ProductRestControllerTest {
+
+    private MockMvc mvc;
 
     @Autowired
     private WebApplicationContext context;
 
-    private MockMvc mvc;
-
     @BeforeEach
     public void setup() {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
+        mvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
     }
 
     @Test
-    void deveFalhar() throws Exception {
+    void naoDevePermitirAcessarProdutosSemAutenticacao() throws Exception {
         mvc.perform(get("/products"))
-            .andExpect(status().isUnauthorized());
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
-    @WithMockUser(value = "joaodasilva", authorities = "ROLE_CUSTOMER")
+    @WithUserDetails("alex")
     void devePermitirUsuarioComRoleAcesseProdutos() throws Exception {
         mvc.perform(get("/products"))
                 .andExpect(status().isOk());
@@ -47,34 +49,15 @@ class ProductRestControllerTest {
     @Test
     @WithUserDetails("joao")
     void naoDevePermitirQueUmUsuarioDePerfilClienteCadastreUmProduto() throws Exception {
-        String requestBody =
-                """
+        String requestBody = """
                 {
-                    "name": "Novo produto"
+                   "name": "Novo produto"
                 }
                 """;
-        mvc.perform(
-                    post("/products")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(requestBody)
-                )
-                .andExpect(status().isForbidden());
+        mvc.perform(post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+        ).andExpect(status().isForbidden());
     }
 
-    @Test
-    @WithUserDetails("alex")
-    void devePermitirQueUmUsuarioDePerfilAdminCadastreUmProduto() throws Exception {
-        String requestBody =
-                """
-                {
-                    "name": "Novo produto"
-                }
-                """;
-        mvc.perform(
-                    post("/products")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(requestBody)
-                )
-                .andExpect(status().isCreated());
-    }
 }
