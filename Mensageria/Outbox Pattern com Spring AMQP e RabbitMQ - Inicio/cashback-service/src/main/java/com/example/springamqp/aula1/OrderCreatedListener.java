3c1d1a45ce6b105da196b266b282190ca88f158d
Alex Silva <alexaugustoti@gmail.com>
2023-07-24 11:29:31 -0300

    Transactional Outbox Pattern

---
+++ Mensageria/Outbox Pattern com Spring AMQP e RabbitMQ - Inicio/cashback-service/src/main/java/com/example/springamqp/aula1/OrderCreatedListener.java
@@ -0,0 +1,16 @@
+package com.example.springamqp.aula1;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderCreatedListener {
+
+    @RabbitListener(queues = "orders.v1.order-created.generate-cashback")
+    public void onOrderCreated(OrderCreatedEvent event) {
+        System.out.println("Id recebido " + event.getId());
+        System.out.println("Valor recebido " + event.getValue());
+    }
+
+}
